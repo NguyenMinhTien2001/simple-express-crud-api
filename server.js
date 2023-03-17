345754bdfb837b7d546fede8a3ce73347023b0ef
NguyenMinhTien2001 <101681231+NguyenMinhTien2001@users.noreply.github.com>
2023-03-17 07:57:33 +0700

    1911067324

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require("express"),
-  bodyParser = require("body-parser");
-
+  const express = require("express"),
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
 const app = express();
 app.use(
   bodyParser.urlencoded({
@@ -10,6 +11,42 @@ app.use(
 app.use(bodyParser.json());
 
 app.use("/books", require("./routes/books"));
+
+
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
